b3635cb613da4e8bd7cb51abb5d1d29275fe6d8a
Tran Ngoc Duc <caothu91@gmail.com>
2024-06-02 17:54:30 +0000

    loop all link in site

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #!/usr/bin/env python
 
+import datetime
 import re
 import sys
 import time
@@ -11,6 +12,13 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+LOGTYPE = {"INFO": "INFO", "ERROR": "ERROR"}
+
+
+def log(mes, log_type=LOGTYPE["INFO"]):
+    print(log_type, datetime.datetime.now(), mes)
+
 
 options = Options()
 firefox_profile = FirefoxProfile()
@@ -25,29 +33,17 @@ driver.get(website_url)
 
 title = driver.title
 print(title)
-print(driver.page_source)
-# This example retrieves only the first 5 links
-for i in range(5):
-    # Find all links using the anchor tag (a)
-    links = driver.find_elements(By.TAG_NAME, "a")
+links = driver.find_elements(By.TAG_NAME, "a")
 
-    # Check if links exist before accessing them
-    if links:
-        #     # Get the first link and its href attribute (URL)
-        link = links[i]
-        link_url = link.get_attribute("href")
+# navigation
 
-        #     # Open the link in the same tab (avoid overwhelming the server)
+for link in links:
+    time.sleep(1)
+    link_url = link.get_attribute("href")
+    if link_url:
+        time.sleep(1)
         driver.get(link_url)
-
-        #     # You can add a short pause here to simulate human-like behavior (optional)
-        # time.sleep(1)
-
-        #     # Print the current URL for reference
         print(f"Visited: {link_url}")
-    else:
-        print("No more links found on this page.")
-        break  # Exit the loop if no links are found
+        # log(f"Visited: {link_url}")
 
-# Close the browser window
 driver.quit()
