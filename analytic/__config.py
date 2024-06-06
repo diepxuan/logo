1e683aa03a98b431d3955817bdcb8426eba07da8
Tran Ngoc Duc <caothu91@gmail.com>
2024-06-06 16:06:27 +0000

    TypeError: descriptor 'append' for 'list' objects doesn't apply to a 'ConfigParser' object

--- analytic/__config.py
+++ analytic/__config.py
@@ -13,22 +13,21 @@ import __os as os
 
 
 def configPath(path):
-    return os.path.join(os.dirImg(item, make=True), "config.ini")
+    return os.path.join(os.dirImg(path, make=True), "config.ini")
 
 
 def set(path, config=configparser.ConfigParser()):
-    global lstConfig
     with open(configPath(path), "w") as configfile:
         config.write(configfile)
     config["DEFAULT"]["path"] = path
-    lstConfig.append(config)
     return config
 
 
 def get(path) -> configparser.ConfigParser:
-    if any(item for item in lstConfig if item["DEFAULT"]["path"] == path):
-        return [item for item in lstConfig if item["DEFAULT"]["path"] == path][0]
-    return set(path)
+    config = configparser.ConfigParser()
+    config.read(configPath(path))
+    config["DEFAULT"]["path"] = path
+    return config
 
 
 def isOld(path) -> bool:
@@ -39,14 +38,3 @@ def isOld(path) -> bool:
         ) < datetime.now() - timedelta(days=2)
     except:
         return True
-
-
-lstConfig = list
-for item in [
-    item
-    for item in os.listdir(os.dirImg(make=True))
-    if os.path.isdir(os.dirImg(item, make=True))
-]:
-    config = configparser.ConfigParser().read(configPath(item))
-    config["DEFAULT"]["path"] = item
-    lstConfig.append(config)
