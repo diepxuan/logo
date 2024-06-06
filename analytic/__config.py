aa9ae2f64252130f3808d3c0e1dcc32dd70db9c0
Tran Ngoc Duc <caothu91@gmail.com>
2024-06-06 15:45:21 +0000

    check this config older 2 days

--- analytic/__config.py
+++ analytic/__config.py
@@ -1,5 +1,4 @@
 import time
-import datetime
 import configparser
 
 from selenium import webdriver
@@ -7,6 +6,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.common.by import By
 from urllib.parse import urlparse
+from datetime import datetime, timedelta
 
 import __string as string
 import __os as os
@@ -29,6 +29,16 @@ def get(path) -> configparser.ConfigParser:
     if any(item for item in lstConfig if item["DEFAULT"]["path"] == path):
         return [item for item in lstConfig if item["DEFAULT"]["path"] == path][0]
     return set(path)
+
+
+def isOld(path) -> bool:
+    config = get(path)
+    try:
+        return datetime.strptime(
+            config["DEFAULT"]["lastOpen"]
+        ) < datetime.now() - timedelta(days=2)
+    except:
+        return True
 
 
 lstConfig = list
