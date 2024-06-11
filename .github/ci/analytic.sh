003679f3dbc73460c4aa9a8b8363d2339889d520
Tran Ngoc Duc <caothu91@gmail.com>
2024-06-11 08:06:59 +0000

    pre-run ci

--- .github/ci/analytic.sh
+++ .github/ci/analytic.sh
@@ -5,8 +5,12 @@ set -e
 # set -u
 
 sudo killall firefox 2>/dev/null || true
+sudo killall google-chrome 2>/dev/null || true
+sudo killall chrome 2>/dev/null || true
 
 export MODE="product"
 python -u analytic || true
 
 sudo killall firefox 2>/dev/null || true
+sudo killall google-chrome 2>/dev/null || true
+sudo killall chrome 2>/dev/null || true
--- .github/ci/search.sh
+++ .github/ci/search.sh
@@ -5,9 +5,13 @@ set -e
 # set -u
 
 sudo killall firefox 2>/dev/null || true
+sudo killall google-chrome 2>/dev/null || true
+sudo killall chrome 2>/dev/null || true
 
 export MODE="product"
 export TYPE="search"
 python -u analytic || true
 
 sudo killall firefox 2>/dev/null || true
+sudo killall google-chrome 2>/dev/null || true
+sudo killall chrome 2>/dev/null || true
