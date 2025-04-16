6f0cc1c5fa082192184c09007e8c49cb72023297
Trần Ngọc Đức <caothu91@gmail.com>
2025-04-16 18:57:16 +0700

    update nameservers

--- .github/ci/facebook.sh
+++ .github/ci/facebook.sh
@@ -10,47 +10,12 @@ set -e
 
 curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=${TS_OAUTH_SECRET} --accept-dns=true --accept-routes=true
 
-# Define the desired nameservers (replace with your preferred servers)
-DNS_SERVER1="10.10.1.253"
-DNS_SERVER2="100.100.100.100"
-
-# Check if necessary tools are installed (resolvectl for netplan systems)
-if ! command -v resolvectl &>/dev/null; then
-    sudo apt install resolvconf
-    # echo "This script requires 'resolvectl' to be installed. Please install it using 'sudo apt install resolvconf'."
-    # exit 1
-fi
-
-# Identify the network interface type (eth0 for wired, wlan0 for wireless - adjust if needed)
-INTERFACE_NAME=$(ip route show default | awk '{print $5}')
-
-# Check if a valid interface is found
-if [ -z "$INTERFACE_NAME" ]; then
-    echo "Error: Could not identify the default network interface."
-    exit 1
-fi
-
-# Check if Netplan is used (systemd-networkd for older systems)
-if [[ -f "/etc/netplan/00-installer-config.yaml" ]]; then
-    # Netplan is used, configure nameservers in YAML
-    echo "Using Netplan for network configuration."
-    sudo sed -i "s/dns-nameservers:.*/dns-nameservers: \['$DNS_SERVER1', '$DNS_SERVER2'\]/g" /etc/netplan/00-installer-config.yaml
-    sudo netplan apply
-else
-    # Not using Netplan, configure nameservers for systemd-networkd
-    echo "Using systemd-networkd for network configuration."
-    sudo bash -c "echo 'nameserver='$DNS_SERVER1'\nnameserver='$DNS_SERVER2'\n' > /etc/resolv.conf"
-    sudo chmod 644 /etc/resolv.conf
-    systemctl status systemd-resolved
-fi
-
-echo "Nameservers set to: $DNS_SERVER1, $DNS_SERVER2"
-
-nslookup ${DB_HOST} || true
-dig mysql.diepxuan.corp || true
-dig @10.10.1.253 mysql.diepxuan.corp || true
-dig @100.100.100.100 mysql.diepxuan.corp || true
-resolvectl status || cat /etc/resolv.conf
+nslookup ${DB_HOST} 10.10.1.253 || true
+nslookup ${DB_HOST} 100.100.100.100 || true
+# dig mysql.diepxuan.corp || true
+# dig @10.10.1.253 mysql.diepxuan.corp || true
+# dig @100.100.100.100 mysql.diepxuan.corp || true
+# resolvectl status || cat /etc/resolv.conf
 
 pip install facebook-scraper mysql-connector-python
 pip install "lxml[html_clean]"
