3fc07e806a8c60639d2a6b6a7f14914d42287948
Trần Ngọc Đức <caothu91@gmail.com>
2025-04-16 18:37:50 +0700

    ISSUES ModuleNotFoundError: No module named 'selenium'

--- analytic/main.py
+++ analytic/main.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
 #!/usr/bin/env python
 
-import datetime
-import re
-import sys
-import time
-
+import importlib
 import __os as os
-import search
-import images
-import videos
-import page
-import facebook
 
+MODULES = {
+    "crawl": "page",
+    "images": "images",
+    "search": "search",
+    "videos": "videos",
+    "facebook": "facebook"
+}
 
 def run_as_type():
     _type = os.environ.get("TYPE", "crawl")
-    if _type == "crawl":
-        page.crawl()
-    elif _type == "images":
-        images.crawl()
-    elif _type == "search":
-        search.crawl()
-    elif _type == "videos":
-        videos.crawl()
-    elif _type == "facebook":
-        facebook.crawl()
-    else:
-        page.crawl()
+    module_name = MODULES.get(_type, "page")
 
+    try:
+        module = importlib.import_module(module_name)
+        # Gọi hàm crawl() nếu module và hàm tồn tại
+        if hasattr(module, "crawl"):
+            module.crawl()
+        else:
+            print(f"❌ Module '{module_name}' không có hàm crawl()")
+    except ModuleNotFoundError:
+        print(f"❌ Module '{module_name}' không tồn tại!")
 
 if __name__ == "__main__":
     run_as_type()
