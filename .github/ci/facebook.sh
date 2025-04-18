d392eb542d4337701f25c4c196f6a59ccdac2193
Trần Ngọc Đức <caothu91@gmail.com>
2025-04-18 17:28:58 +0700

    debug log get fb post

--- .github/ci/facebook.sh
+++ .github/ci/facebook.sh
@@ -4,30 +4,9 @@
 set -e
 # set -u
 
-# sudo killall firefox 2>/dev/null || true
-# sudo killall google-chrome 2>/dev/null || true
-# sudo killall chrome 2>/dev/null || true
-
-curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=${TS_OAUTH_SECRET} --accept-dns=true --accept-routes=true
-tailscale status
-ip a
-ip r
-
-nslookup ${DB_HOST} 10.10.1.253 || true
-nslookup ${DB_HOST} 100.100.100.100 || true
-# dig mysql.diepxuan.corp || true
-# dig @10.10.1.253 mysql.diepxuan.corp || true
-# dig @100.100.100.100 mysql.diepxuan.corp || true
-# resolvectl status || cat /etc/resolv.conf
-
 pip install facebook-scraper mysql-connector-python
 pip install "lxml[html_clean]"
 
 export TYPE="facebook"
 python -u analytic || true
 
-sudo tailscale logout
-
-# sudo killall firefox 2>/dev/null || true
-# sudo killall google-chrome 2>/dev/null || true
-# sudo killall chrome 2>/dev/null || true
--- analytic/facebook.py
+++ analytic/facebook.py
@@ -7,25 +7,25 @@ import __os as os
 FB_PAGE = "Everonpage"  # Thay bằng page bạn cần lấy
 NUM_PAGES = 1
 
-db = mysql.connector.connect(
-    host=os.environ.get("DB_HOST", "mysql.diepxuan.corp"),
-    user=os.environ.get("DB_USERNAME", "n8n"),
-    password=os.environ.get("DB_PASSWORD"),
-    database=os.environ.get("DB_DATABASE", "n8n"),
-    port=os.environ.get("DB_PORT", 3306)
-)
+# db = mysql.connector.connect(
+#     host=os.environ.get("DB_HOST", "mysql.diepxuan.corp"),
+#     user=os.environ.get("DB_USERNAME", "n8n"),
+#     password=os.environ.get("DB_PASSWORD"),
+#     database=os.environ.get("DB_DATABASE", "n8n"),
+#     port=os.environ.get("DB_PORT", 3306)
+# )
 
 def crawl():
-    cursor = db.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS fb_posts (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            post_id VARCHAR(50) UNIQUE,
-            text TEXT,
-            post_time DATETIME,
-            link TEXT
-        );
-    """)
+    # cursor = db.cursor()
+    # cursor.execute("""
+    #     CREATE TABLE IF NOT EXISTS fb_posts (
+    #         id INT AUTO_INCREMENT PRIMARY KEY,
+    #         post_id VARCHAR(50) UNIQUE,
+    #         text TEXT,
+    #         post_time DATETIME,
+    #         link TEXT
+    #     );
+    # """)
 
     for post in get_posts(FB_PAGE, pages=NUM_PAGES):
         post_id = post.get("post_id")
@@ -33,12 +33,18 @@ def crawl():
         time = post.get("time")
         link = post.get("post_url")
 
-        cursor.execute("""
-            INSERT INTO fb_posts (post_id, text, post_time, link)
-            VALUES (%s, %s, %s, %s)
-            ON DUPLICATE KEY UPDATE text = VALUES(text), post_time = VALUES(post_time), link = VALUES(link)
-        """, (post_id, text, time, link))
+        # cursor.execute("""
+        #     INSERT INTO fb_posts (post_id, text, post_time, link)
+        #     VALUES (%s, %s, %s, %s)
+        #     ON DUPLICATE KEY UPDATE text = VALUES(text), post_time = VALUES(post_time), link = VALUES(link)
+        # """, (post_id, text, time, link))
+        print("Đã lấy bài viết:", post_id)
+        print("Nội dung:", text)
+        print("Thời gian:", time)
+        print("Link:", link)
+        print("=====================================")
+        # Lưu vào database
 
-    db.commit()
-    db.close()
+    # db.commit()
+    # db.close()
     print("✅ Lưu thành công các bài viết!")
